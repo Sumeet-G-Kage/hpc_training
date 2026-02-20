ccc3c34dbab46cf590167d31221ee1b0858867a2
Sumeet_G_Kage <sumeetkage23@gmail.com>
2026-02-20 04:11:21 +0000

    Added Session 2 task2 C code

---
+++ mpi_programs/session2_task2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char **argv)
+{
+    int rank, nprocs,s_data, r_data,dest, source;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    s_data = rank;
+
+    // use only this formula for next process
+    dest = (rank + 1) % nprocs;
+
+    // previous process without modulo
+    if(rank == 0)
+        source = nprocs - 1;
+    else
+        source = rank - 1;
+
+    if(rank == 0)
+    {
+        MPI_Send(&s_data, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+        MPI_Recv(&r_data, 1, MPI_INT, source, 0,
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+    else
+    {
+        MPI_Recv(&r_data, 1, MPI_INT, source, 0,
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Send(&s_data, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+    }
+
+    printf("Rank %d received %d\n", rank, r_data);
+
+    MPI_Finalize();
+    return 0;
+}
